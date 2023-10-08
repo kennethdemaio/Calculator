c5a9733954f507eeec766ccd1772f27ed1a2410d
Kenneth DeMaio <kmd2000main@Hotmail.com>
2023-10-08 15:17:42 -0400

    Loop added to run again

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,34 +1,45 @@
-﻿Console.WriteLine("---------------------------");
-Console.WriteLine("Welcome to my C# calculator");
-Console.WriteLine("---------------------------");
+﻿bool run = true;
+while (run){
+    Console.WriteLine("---------------------------");
+    Console.WriteLine("Welcome to my C# calculator");
+    Console.WriteLine("---------------------------");
 
-Console.WriteLine("Input 1");
-string InputOne = Console.ReadLine();
-Console.WriteLine("Input 2");
-string InputTwo = Console.ReadLine();
+    Console.WriteLine("Please enter your first input");
+    string InputOne = Console.ReadLine();
+    Console.WriteLine("Please enter your second input");
+    string InputTwo = Console.ReadLine();
 
-var InputOneConverted = Convert.ToInt32(InputOne);
-var InputTwoConverted = Convert.ToInt32(InputTwo);
+    var InputOneConverted = Convert.ToInt32(InputOne);
+    var InputTwoConverted = Convert.ToInt32(InputTwo);
 
-Console.WriteLine("Would you like to multiply (*), divide (/), subtract(-), or add(+)?");
-string InputOperator = Console.ReadLine();
+    Console.WriteLine("Would you like to multiply (*), divide (/), subtract(-), or add(+)?");
+    string InputOperator = Console.ReadLine();
 
-switch (InputOperator){
-    case "+":
-        int AddResult = InputOneConverted + InputTwoConverted;
-        Console.WriteLine(AddResult);
-        break;
-    case "-":
-        int SubResult = InputOneConverted - InputTwoConverted;
-        Console.WriteLine(SubResult);
-        break;
-    case "*":
-        int MultiplyResult = InputOneConverted + InputTwoConverted;
-        Console.WriteLine(MultiplyResult);
-        break;
-    case "/":
-        int DivideResult = InputOneConverted + InputTwoConverted;
-        Console.WriteLine(DivideResult);
-        break;
+    switch (InputOperator){
+        case "+":
+            int AddResult = InputOneConverted + InputTwoConverted;
+            Console.WriteLine(AddResult);
+            break;
+        case "-":
+            int SubResult = InputOneConverted - InputTwoConverted;
+            Console.WriteLine(SubResult);
+            break;
+        case "*":
+            int MultiplyResult = InputOneConverted + InputTwoConverted;
+            Console.WriteLine(MultiplyResult);
+            break;
+        case "/":
+            int DivideResult = InputOneConverted + InputTwoConverted;
+            Console.WriteLine(DivideResult);
+            break;
+    }
+    Console.WriteLine("Would you like to calculate another value? (Y/N)");
+    string CalAgain = Console.ReadLine();
+    if(CalAgain == "Y"){
+        run = true;
+    }
+    else
+    {
+        run = false;
+    }
 }
-
