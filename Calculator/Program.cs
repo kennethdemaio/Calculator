f99bd856acec843e451ffafc032cd1338ddc6427
Kenneth DeMaio <kmd2000main@Hotmail.com>
2023-10-08 15:34:57 -0400

    Fixed major logic issues

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,5 +1,5 @@
 ﻿bool run = true;
-while (run){
+while (run) {
     Console.WriteLine("---------------------------");
     Console.WriteLine("Welcome to my C# calculator");
     Console.WriteLine("---------------------------");
@@ -9,42 +9,65 @@ while (run){
     Console.WriteLine("Please enter your second input");
     string InputTwo = Console.ReadLine();
 
-    var InputOneConverted = Convert.ToInt32(InputOne);
-    var InputTwoConverted = Convert.ToInt32(InputTwo);
-
-    Console.WriteLine("Would you like to multiply (*), power of (**), divide (/), subtract(-), or add(+)?");
-    string InputOperator = Console.ReadLine();
-
-    switch (InputOperator){
-        case "+":
-            int AddResult = InputOneConverted + InputTwoConverted;
-            Console.WriteLine(AddResult);
-            break;
-        case "-":
-            int SubResult = InputOneConverted - InputTwoConverted;
-            Console.WriteLine(SubResult);
-            break;
-        case "*":
-            int MultiplyResult = InputOneConverted * InputTwoConverted;
-            Console.WriteLine(MultiplyResult);
-            break;
-        case "/":
-            int DivideResult = InputOneConverted / InputTwoConverted;
-            Console.WriteLine(DivideResult);
-            break;
-        case "**":
-            double PowerResult = Math.Pow(InputOneConverted, InputTwoConverted);
-            Console.WriteLine(PowerResult);
-            break;
-    }
-    Console.WriteLine("Would you like to calculate another value? (Y/N)");
-    string CalAgain = Console.ReadLine();
-    if(CalAgain == "Y"){
-        run = true;
+    if (!int.TryParse(InputOne, out var InputOneConverted) || !int.TryParse(InputTwo, out var InputTwoConverted))
+    {
+        Console.WriteLine("Invalid input. Please enter valid numbers.");
+        continue; // Restart the loop to get valid input
     }
     else
     {
-        Console.WriteLine("Have a nice day!");
-        run = false;
+        Calculate(InputOne, InputTwo);
+    }
+
+
+    static void Calculate(string InputOne, string InputTwo){
+        var InputOneConverted = Convert.ToInt32(InputOne);
+        var InputTwoConverted = Convert.ToInt32(InputTwo);
+
+        Console.WriteLine("Would you like to multiply (*), power of (**), divide (/), subtract(-), or add(+)?");
+        string InputOperator = Console.ReadLine();
+
+        switch (InputOperator) {
+            case "+":
+                int AddResult = InputOneConverted + InputTwoConverted;
+                Console.WriteLine(AddResult);
+                break;
+            case "-":
+                int SubResult = InputOneConverted - InputTwoConverted;
+                Console.WriteLine(SubResult);
+                break;
+            case "*":
+                int MultiplyResult = InputOneConverted * InputTwoConverted;
+                Console.WriteLine(MultiplyResult);
+                break;
+            case "/":
+                int DivideResult = InputOneConverted / InputTwoConverted;
+                Console.WriteLine(DivideResult);
+                break;
+            case "**":
+                double PowerResult = Math.Pow(InputOneConverted, InputTwoConverted);
+                Console.WriteLine(PowerResult);
+                break;
+        }
+    }
+    bool QuestAgain = true;
+    while (QuestAgain)
+    {
+        Console.WriteLine("Would you like to calculate another value? (Y/N)");
+        string CalAgain = Console.ReadLine();
+        if (CalAgain == "Y")
+        {
+            run = true;
+        }
+        else if (CalAgain == "N")
+        {
+            Console.WriteLine("Have a nice day!");
+            run = false;
+        }
+        else
+        {
+            Console.WriteLine("Please input a valid response");
+            QuestAgain = true;
+        }
     }
 }
