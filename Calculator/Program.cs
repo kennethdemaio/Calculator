35a01ebbb3de9e9fc5af62b2b9dc27d64faf71de
Kenneth DeMaio <kmd2000main@Hotmail.com>
2023-10-08 15:13:18 -0400

    Early stages of math

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,5 +1,34 @@
-﻿Console.WriteLine("Github test");
+﻿Console.WriteLine("---------------------------");
+Console.WriteLine("Welcome to my C# calculator");
+Console.WriteLine("---------------------------");
 
-Console.WriteLine("Second test");
+Console.WriteLine("Input 1");
+string InputOne = Console.ReadLine();
+Console.WriteLine("Input 2");
+string InputTwo = Console.ReadLine();
 
-Console.WriteLine("third test");
+var InputOneConverted = Convert.ToInt32(InputOne);
+var InputTwoConverted = Convert.ToInt32(InputTwo);
+
+Console.WriteLine("Would you like to multiply (*), divide (/), subtract(-), or add(+)?");
+string InputOperator = Console.ReadLine();
+
+switch (InputOperator){
+    case "+":
+        int AddResult = InputOneConverted + InputTwoConverted;
+        Console.WriteLine(AddResult);
+        break;
+    case "-":
+        int SubResult = InputOneConverted - InputTwoConverted;
+        Console.WriteLine(SubResult);
+        break;
+    case "*":
+        int MultiplyResult = InputOneConverted + InputTwoConverted;
+        Console.WriteLine(MultiplyResult);
+        break;
+    case "/":
+        int DivideResult = InputOneConverted + InputTwoConverted;
+        Console.WriteLine(DivideResult);
+        break;
+}
+
